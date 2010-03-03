60b1cab08ab44f690aeae7d1950e0d7c0e761295
Brice Figureau <brice-puppet@daysofwonder.com>
2010-03-03 20:35:46 +0100

    Fix #3229 - use original value in case/selector regex matching
    The issue is that case/selectors are downcasing the value before it
    is compared to the options.
    Unfortunately regex are matching in a case sensitive way, which would
    make the following manifest fail:
    
    $var = "CaseSensitive"
    case $var {
      /CaseSensitive/: {
         notice("worked")
      }
      default: {
        fail "miserably"
      }
    }
    
    This patch fixes the issue by making sure the regexp match is done
    one the original (not downcased) value, but still doing a case
    sensitive match.
    
    Signed-off-by: Brice Figureau <brice-puppet@daysofwonder.com>

--- spec/unit/parser/ast/selector.rb
+++ spec/unit/parser/ast/selector.rb
@@ -42,16 +42,6 @@ describe Puppet::Parser::AST::Selector do
             @selector.evaluate
         end
 
-        it "should downcase the evaluated param value if allowed" do
-            Puppet.stubs(:[]).with(:casesensitive).returns(false)
-            value = stub 'param'
-            @param.stubs(:safeevaluate).returns(value)
-
-            value.expects(:downcase)
-
-            @selector.evaluate
-        end
-
         it "should scan each option" do
             @values.expects(:each).multiple_yields(@value1, @value2)
 
