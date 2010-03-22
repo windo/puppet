a091f011b6311dd37c1a2a92a447d2a90d7a22fb
Markus Roberts <Markus@reality.com>
2010-03-21 21:14:12 -0700

    Code smell: Use ||= for conditional initialization
    Replaced 57 occurances of
    
        ([$@]?\w+) += +(.*) +(if +\1.nil\?|if +! *\1|unless +\1|unless +defined\?\(\1\))$
    
    with
    
        \1 ||= \2
    
    3 Examples:
    
        The code:
            @sync
        becomes:
            @sync
        The code:
    
        becomes:
    
        The code:
            @sample_crons
        becomes:
            @sample_crons

--- lib/puppet/parser/resource.rb
+++ lib/puppet/parser/resource.rb
@@ -28,7 +28,7 @@ class Puppet::Parser::Resource < Puppet::Resource
 
     # Determine whether the provided parameter name is a relationship parameter.
     def self.relationship_parameter?(name)
-        @relationship_names = Puppet::Type.relationship_params.collect { |p| p.name } unless defined?(@relationship_names)
+        @relationship_names ||= Puppet::Type.relationship_params.collect { |p| p.name }
         @relationship_names.include?(name)
     end
 
@@ -145,7 +145,7 @@ class Puppet::Parser::Resource < Puppet::Resource
     # Return the resource name, or the title if no name
     # was specified.
     def name
-        @name = self[:name] || self.title unless defined?(@name)
+        @name ||= self[:name] || self.title
         @name
     end
 
