b98645e43e07593a7a05e5528a98a0040e1b0f01
Markus Roberts <Markus@reality.com>
2010-03-21 21:14:12 -0700

    Code smell: Omit needless checks on defined
    * Replaced 56 occurances of
    
          defined\?\((.+?)\) (?:and|&&) \1( |$)
    
      with
    
          \1\2
    
      In code like:
    
         unless defined? @foo and @foo and bar("baz")
    
      "defined? @foo and @foo" can safely be replaced with "@foo":
    
         unless @foo and bar("baz")
    
      Because:
    
       * Both evaluate to false/nil when @foo is not defined
       * Both evaluate to @foo when @foo is defined
    
      3 Examples:
    
          The code:
              @sync = Sync.new unless defined?(@sync) and @sync
          becomes:
              @sync = Sync.new unless @sync
          The code:
              unless defined?(@content) and @content
          becomes:
              unless @content
          The code:
              raise(ArgumentError, "Already handling indirection for #{@indirection.name}; cannot also handle #{indirection}") if defined?(@indirection) and @indirection
          becomes:
              raise(ArgumentError, "Already handling indirection for #{@indirection.name}; cannot also handle #{indirection}") if @indirection
    
    * Replaced 2 occurances of
    
          defined\?\((.+?)\) (?:and|&&) ! *\1.nil\?
    
      with
    
          !\1.nil?
    
      In code like:
    
         while defined? @foo and ! @foo.nil? ...
    
      "defined? @foo and ! @foo.nil?" can safely be replaced with "! @foo.nil?":
    
         while ! @foo.nil? ...
    
      Because:
    
       * Both evaluate to false/nil when @foo is not defined
       * Both evaluate to "! @foo.nil?" when @foo is defined
    
      2 Examples:
    
          The code:
              return !!(defined?(@value) and ! @value.nil?)
          becomes:
              return !!(!@value.nil?)
          The code:
              self.init unless defined?(@@state) and ! @@state.nil?
          becomes:
              self.init unless !@@state.nil?

--- lib/puppet/parameter.rb
+++ lib/puppet/parameter.rb
@@ -97,7 +97,7 @@ class Puppet::Parameter
 
         # Is this parameter the namevar?  Defaults to false.
         def isnamevar?
-            return defined?(@isnamevar) && @isnamevar
+            return @isnamevar
         end
 
         # This parameter is required.
@@ -112,7 +112,7 @@ class Puppet::Parameter
 
         # Is this parameter required?  Defaults to false.
         def required?
-            return defined?(@required) && @required
+            return @required
         end
 
         # Verify that we got a good value
@@ -171,9 +171,9 @@ class Puppet::Parameter
 
         error = type.new(args.join(" "))
 
-        error.line = @resource.line if defined?(@resource) and @resource and @resource.line
+        error.line = @resource.line if @resource and @resource.line
 
-        error.file = @resource.file if defined?(@resource) and @resource and @resource.file
+        error.file = @resource.file if @resource and @resource.file
 
         raise error
     end
@@ -219,7 +219,7 @@ class Puppet::Parameter
     # return the full path to us, for logging and rollback; not currently
     # used
     def pathbuilder
-        if defined?(@resource) and @resource
+        if @resource
             return [@resource.pathbuilder, self.name]
         else
             return [self.name]
